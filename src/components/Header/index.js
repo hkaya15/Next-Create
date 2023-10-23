83fe9a72e304661ff21059c0b03b5da332475a23
hkaya15 <kayahuseyin15@gmail.com>
2023-10-23 17:33:03 +0300

    Add Link component with object type href

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from './styles.module.css'
-
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.css";
 
 function Header() {
   return (
     <header className={styles.header}>
-        <Link href='/' className={styles.logo}>
-            <span>BLOG</span>
+      <Link href="/" className={styles.logo}>
+        <span>BLOG</span>
+      </Link>
+      <nav>
+        <Link href="/about" target="_blank">
+          About
         </Link>
-        <nav>
-            <Link href='/about' target='_blank'>
-                About
-            </Link>
-        </nav>
+        <hr></hr>
+        <Link
+          href={{
+            pathname: "/obje",
+            query: {
+              name: "Next.js",
+            },
+            // If you would like to see suggestions about href object you can push Option + Esc on Mac, Alt + Esc on Windows.
+          }}
+        >
+          Obje
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
