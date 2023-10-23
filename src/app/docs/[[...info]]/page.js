e0280d7224723c0c7f2d97e0597c21eb2dc6882d
hkaya15 <kayahuseyin15@gmail.com>
2023-10-23 15:23:30 +0300

    Add optional catch feature

---
+++ src/app/docs/[[...info]]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function InfoPageWithOptionalCatch({params}) {
+  return (
+    <div>InfoPageWithOptionalCatch - {params.info}</div>
+  )
+}
+
+export default InfoPageWithOptionalCatch
