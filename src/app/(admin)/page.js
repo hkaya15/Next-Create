583060a7c425c87268f620a2887f41c1c6310a73
hkaya15 <kayahuseyin15@gmail.com>
2023-10-23 17:00:19 +0300

    Add route grouping feature

---
+++ src/app/(admin)/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AdminPage() {
+  return (
+    <div>AdminPage</div>
+  )
+}
+
+export default AdminPage
+
+
+// () => helps to create grouping for management. It doesn't effect the routing drilling.
